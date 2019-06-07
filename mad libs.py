28a3632b9678ed5a7e1db15cc0bed706b6fc36d1
Eshwar0601 <40708201+Eshwar0601@users.noreply.github.com>
2019-06-07 09:31:19 -0400

    Add files via upload

---
+++ mad libs.py
@@ -0,0 +1,17 @@
+# mad libs game 
+
+
+blank1=input("Enter a name:")
+blank2=input("Enter a place:")
+blank3=input("Enter a thing:") 
+blank4=input("Enter a animal:") 
+blank5=input("Enter a relation(like brother, sister,......):") 
+blank6=input("Enter a preposition:") 
+blank7=input("Enter a name (different from the one above):") 
+
+print("The story is as follows")
+print("=============================================================")
+print(blank1," went to ",blank2,".Here he saw a ",blank4," in order to protect himself,he killed that "
+,blank4,"with an ",blank3,", but the ",blank5,"of ",blank4," found him and chased "
+,blank1,"untill he found a hiding spot ",blank6," the lake , and he was kidnaped by ",blank7,".")
+
